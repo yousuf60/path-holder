b0609837e0b9685e57697981d97000b0124f370c
yousufmh60 <yosefmahmoud356@gmail.com>
2025-08-21 19:02:19 +0300

    update read&write

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ class File:
         self.hold(*paths)
             
     def make(self):
-        if self.ok():
-            return True
         self.file.touch()
 
     def remove(self):
@@ -30,4 +28,25 @@ class File:
         self.hold(splitted_path[0], one_file)
     
         
+    def _open(self, *args, **kwargs):
+        return open(self.file, *args, **kwargs)
 
+        
+    def write(self, text: str, *args, **kwargs):
+        with  self._open("w" ,*args, **kwargs) as f:
+            f.write(str(text))
+
+    def add(self, text: str, *args, **kwargs):
+        with self._open("a", *args, **kwargs) as f:
+            f.write(text)
+            
+    def clear(self):
+        self.write("")
+        
+    def lines(self):
+        with self._open("r") as f:
+            yield f.readline()
+
+    def text(self):
+        with self._open("r") as f:
+            return f.read()
--- tests/test_main.py
+++ tests/test_main.py
@@ -12,4 +12,20 @@ def test_remake():
     f.new("f2.txt")
     assert f.ok()
     assert f.file == File("tests/data/f2.txt").file
+    assert f.remove()
+    f.new("f1.txt")
+    assert f.ok()
+
+
+def test_process_file():
+    text1 = "blah blah test"
+    text2 = "2blah blah test"
+    f.write(text1)
+    f.add(text2)
+    assert f.text() == text1+text2
+    assert next(f.lines())== text1+text2
+    f.clear()
+    assert f.text() == ""
+    
+
 
